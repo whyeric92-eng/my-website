cb164a11ff3c61d391ea992fa4c99f7f5afa564d
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-07-14 11:53:12 +0800

    feat: add the contact footer

---
+++ data/contact.ts
@@ -0,0 +1,11 @@
+export type Contact = {
+    email: string;
+    linkedin: string;
+    phoneNumber: string;
+}
+
+export const contact: Contact = {
+    email: "C250227@e.ntu.edu.sg",
+    linkedin: "https://www.linkedin.com/in/haoyu-wei-2673053aa/",
+    phoneNumber: "+65 93618470",
+}
